3b55f56b9dc6c5bfe334964c364a73f325f37e2b
mmenaem <mohamedmenam409@gmail.com>
2022-10-26 06:34:04 +0200

    2 commit

--- genres.js
+++ genres.js
@@ -3,6 +3,7 @@ const exprss=require('express')
 const app=exprss()
 app.use(exprss.json())
 
+// make validation function by use JOI
 function genresValidation(genre){
     const schema=Joi.object({
         name:Joi.string().min(3).required()
@@ -20,11 +21,14 @@ const genres=[
 
 
 app.get('/api/genres',(req,res)=>{
+    // return all genres 
     res.send(genres);
 });
 
 app.get('/api/genres/:id',(req,res)=>{
+    // look up to the genre
     const genre =genres.find(c=>c.id===parseInt(req.params.id));
+    // if not existing return 404
     if(!genre){
         return res.status(400).send("the genre not found !")
     };
@@ -32,16 +36,19 @@ app.get('/api/genres/:id',(req,res)=>{
 });
 
 app.post('/api/genres',(req,res)=>{
-    
+    // validate
     const {error}=genresValidation(req.body)
     if (error){
         return res.status(404).send(error.details[0].message)
     }
+    //create genre
     const genre={
         id:genres.length+1,
         name:req.body.name
     }
+    // uploade the new gener
     genres.push(genre)
+    // return the new gener
     res.send(genre)
 });
 
